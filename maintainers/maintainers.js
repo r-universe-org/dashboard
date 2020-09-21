ec359448549560c93578b5693cdc2f7df92469f7
Jeroen Ooms <jeroenooms@gmail.com>
2020-09-21 23:29:34 +0200

    fix case sensitive comparisons

--- maintainers/maintainers.js
+++ maintainers/maintainers.js
@@ -22,21 +22,22 @@ $(function(){
 		}
 		combine_duplicates(x).sort(order).forEach(function(maintainer){
 			var organizations = {};
+			var login = maintainer.login || "";
 			maintainer.packages.forEach(function(pkg){
 				if(pkg.user == 'test') return;
 				organizations[pkg.user] = organizations[pkg.user] ? organizations[pkg.user] + 1 : 1;
 			});
 			var profile = $("#templatezone .maintainer-profile").clone();
-			if(maintainer.login){
-				profile.find(".maintainer-image").attr('data-src', 'https://github.com/' + maintainer.login + ".png")
-				profile.find(".maintainer-homepage").attr('href', 'https://github.com/' + maintainer.login)
+			if(login){
+				profile.find(".maintainer-image").attr('data-src', 'https://github.com/' + login + ".png")
+				profile.find(".maintainer-homepage").attr('href', 'https://github.com/' + login)
 			}
 			profile.find(".maintainer-name").text(maintainer.name);
 			profile.find(".maintainer-more").text(maintainer.email);
 			var total = 0;
 			for (const [org, count] of Object.entries(organizations)) {
 				total = total + count;
-				if(maintainer.login != org){
+				if(login.toLowerCase() != org.toLowerCase()){
 					var icon = $("<img/>").addClass("lazyload maintainer-org-icon border border-light rounded m-2").attr('src', 'https://github.com/' + org + ".png?size=60").width(30);
 					profile.find(".maintainer-organizations").append(icon);
 				}		
