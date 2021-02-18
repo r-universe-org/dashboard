500ad7b3ea84fdad939af9b49e51ecab885adbe1
Jeroen Ooms <jeroenooms@gmail.com>
2021-02-18 14:10:45 +0100

    better dates

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -269,8 +269,8 @@ function init_article_list(server){
                 item.find('.article-title').text(pkg.vignette.title);
                 item.find('.article-package-version').text(pkg.package + " " + pkg.version);
                 item.find('.article-author-name').text(pkg.maintainer.split("<")[0]);
-                item.find('.article-modified').text('Last update: ' + pkg.vignette.modified.substring(0, 16));
-                item.find('.article-created').text('Started: ' + pkg.vignette.created.substring(0, 16));
+                item.find('.article-modified').text('Last update: ' + pkg.vignette.modified.substring(0, 10));
+                item.find('.article-created').text('Article started: ' + pkg.vignette.created.substring(0, 10));
                 if(pkg.maintainerlogin){
                   item.find('.maintainer-avatar').attr('src', 'https://github.com/' + pkg.maintainerlogin + '.png?size=140');
                 }
