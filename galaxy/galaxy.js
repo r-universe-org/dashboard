a2e4a96956623dcb4f13b99d638f7921fcc4b667
Jeroen Ooms <jeroenooms@gmail.com>
2021-06-08 17:46:47 +0200

    procrastination

---
+++ galaxy/galaxy.js
@@ -0,0 +1,28 @@
+$(function(){
+  function rnd(max){
+    return Math.floor(Math.random() * max);
+  }
+  get_ndjson('https://r-universe.dev/stats/organizations').then(function(x){
+    const stars = $("#stars")[0];
+    function order( a, b ) {
+      if(a.packages.length < b.packages.length) return 1;
+      if(a.packages.length > b.packages.length) return -1;
+      return 0;
+    }
+    x.sort(order).forEach(function(org){
+      var organization = org.organization;
+      if(organization == 'test' || organization.includes('gitlab.com')) return;
+      var size = org.packages.length;
+      var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      var link = document.createElementNS("http://www.w3.org/2000/svg", "a");
+      link.setAttributeNS(null, "href",  'https://' + organization + '.r-universe.dev');
+      circle.setAttributeNS(null, "cx", rnd(100) + "%");
+      circle.setAttributeNS(null, "cy", rnd(100) + "%");
+      circle.setAttributeNS(null, "r",  Math.sqrt(Math.sqrt(size)));
+      circle.style.animationDelay = rnd(100)/10 +"s";
+      $(circle).tooltip({title: organization});
+      link.appendChild(circle);
+      stars.appendChild(link);
+    });
+  });
+});
