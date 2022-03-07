bff96e36960d4cc5ca380b43d52fe4c58e74a995
Jeroen Ooms <jeroenooms@gmail.com>
2022-03-07 13:27:54 +0100

    truncate activity chart at 1y

--- activity/activity.js
+++ activity/activity.js
@@ -12,6 +12,29 @@ function get_ndjson(path){
   return get_path(path).then(txt => txt.split('\n').filter(x => x.length).map(JSON.parse));
 }
 
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+Date.prototype.getWeekYear = function() {
+  var date = new Date(this.getTime());
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  return date.getFullYear();
+}
+
+Date.prototype.yyyymm = function(){
+  const wk = this.getWeek();
+  return this.getWeekYear() + '-' + (wk < 10 ? '0' + wk : wk);
+}
+
 function sort_packages(array){
   return array.sort((a, b) => (a.count > b.count) ? -1 : 1);
 }
@@ -20,20 +43,34 @@ function objectToArray(obj){
   return Object.keys(obj).map(function(key){return {package:key, count: obj[key]}});
 }
 
+function activity_data(updates){
+  const now = new Date();
+  const weeks = Array(53).fill(0).map((_, i) => new Date(now - i*604800000).yyyymm()).reverse();
+  return weeks.map(function(weekval){
+    var out = {
+      year : weekval.split('-')[0],
+      week : parseInt(weekval.split('-')[1])
+    };
+    var rec = updates.find(x => x.week == weekval);
+    if(rec){
+      out.total = rec.total;
+      out.packages = sort_packages(objectToArray(rec.packages)).map(x => x.package);
+    }
+    return out;
+  });
+}
+
 function make_activity_chart(universe){
   return get_ndjson(`https://${universe && universe + "." || ""}r-universe.dev/stats/updates`).then(function(updates){
-    updates.shift();
-    const weeks = updates.map(x => parseInt(x.week.split('-')[1]));
-    const years = updates.map(x => x.week.split('-')[0]);
-    const counts = updates.map(x => sort_packages(objectToArray(x.packages)).map(x => x.package));
+    const data = activity_data(updates);
     const ctx = document.getElementById('activity-canvas');
     const myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: weeks,
+        labels: data.map(x => x.week),
         datasets: [{
           label: 'updates',
-          data: updates.map(x => x.total),
+          data: data.map(x => x.total),
           backgroundColor: 'rgb(54, 162, 235, 0.2)',
           borderColor: 'rgb(54, 162, 235, 1)',
           borderWidth: 2
@@ -53,10 +90,11 @@ function make_activity_chart(universe){
             callbacks: {
               title: function(items){
                 const item = items[0];
-                return years[item.dataIndex] + ' week ' + item.label; 
+                const weekdata = data[item.dataIndex];
+                return weekdata.year + ' week ' + weekdata.week;
               },
               label: function(item) {
-                let packages = counts[item.dataIndex];
+                let packages = data[item.dataIndex].packages;
                 let len = packages.length;
                 if(len > 5){
                   return ` Updates in ${packages.slice(0,4).join(', ')} and ${packages.length-4} other packages`;
