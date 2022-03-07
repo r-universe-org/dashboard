53a3d35bd0ba505e665c4f83a2c5136848873500
Jeroen Ooms <jeroenooms@gmail.com>
2022-03-07 22:36:03 +0100

    search for topics verbatim

--- search/search.js
+++ search/search.js
@@ -59,7 +59,7 @@ $(function(){
       var topicdiv = item.find('.description-topics').removeClass('d-none');
       if(typeof topics === 'string') topics = [topics]; //hack for auto-unbox bug
       topics.filter(x => skiptopics.indexOf(x) < 0).forEach(function(topic){
-        var topicurl = `https://r-universe.dev/search#${topic}`;
+        var topicurl = `https://r-universe.dev/search#"${topic}"`;
         $("<a>").attr("href", topicurl).addClass('badge badge-info mr-1').text(topic).appendTo(topicdiv);
       });
     }
