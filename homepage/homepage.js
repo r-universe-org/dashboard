b65bb005ec565cc0752a04911c56a941203d0c51
Jeroen Ooms <jeroenooms@gmail.com>
2022-04-11 23:08:08 +0200

    bump dashboards to R-4.2

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -151,7 +151,7 @@ function run_icon(run, src){
 }
 
 function is_success(run){
-  return run && run.status && run.status.match(/succ/i);
+  return run && run.status && run.status.match(/succ/i) || run.type == 'pending';
 }
 
 function all_ok(runs){
@@ -239,10 +239,10 @@ function init_packages_table(server, user){
       }
       var name = pkg.package;
       var src = pkg.runs && pkg.runs.find(x => x.type == 'failure') || pkg.runs.find(x => x.type == 'src') || {};
-      var win = pkg.runs && pkg.runs.find(x => x.type == 'win' && x.built.R.substring(0,3) == '4.1') || {skip: pkg.os_restriction === 'unix'}; //{type:'pending'};
-      var mac = pkg.runs && pkg.runs.find(x => x.type == 'mac' && x.built.R.substring(0,3) == '4.1') || {skip: pkg.os_restriction === 'windows'}; //{type:'pending'};
-      var oldwin = pkg.runs && pkg.runs.find(x => x.type == 'win' && x.built.R.substring(0,3) == '4.0') || {skip: pkg.os_restriction === 'unix'};
-      var oldmac = pkg.runs && pkg.runs.find(x => x.type == 'mac' && x.built.R.substring(0,3) == '4.0') || {skip: pkg.os_restriction === 'windows'};
+      var win = pkg.runs && pkg.runs.find(x => x.type == 'win' && x.built.R.substring(0,3) == '4.2') || {skip: pkg.os_restriction === 'unix'}; //{type:'pending'};
+      var mac = pkg.runs && pkg.runs.find(x => x.type == 'mac' && x.built.R.substring(0,3) == '4.2') || {type:'pending'} // {skip: pkg.os_restriction === 'windows'}; //{type:'pending'};
+      var oldwin = pkg.runs && pkg.runs.find(x => x.type == 'win' && x.built.R.substring(0,3) == '4.1') || {skip: pkg.os_restriction === 'unix'};
+      var oldmac = pkg.runs && pkg.runs.find(x => x.type == 'mac' && x.built.R.substring(0,3) == '4.1') || {skip: pkg.os_restriction === 'windows'};
       var builddate = new Date(src.date || NaN).yyyymmdd();
       var commiturl = `${pkg.upstream}/commit/${pkg.commit}`;
       var versionlink = $("<a>").text(pkg.version).attr("href", commiturl).attr("target", "_blank").addClass('text-dark');
