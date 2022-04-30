945fcd88cf6269303342d9a6a7ecde84da8e0ec0
Jeroen Ooms <jeroenooms@gmail.com>
2022-04-30 17:56:12 +0200

    add an icon

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1253,7 +1253,7 @@ function populate_package_details(package){
         add_one_contributor();
       }
       if(names.length){
-        var morelink = $('<a href="#">').addClass("btn btn-sm btn-outline-primary m-2").text(`Show all... (${total})`).click(function(e){
+        var morelink = $(`<button class="btn btn-sm btn-outline-primary m-2"><i class="fas fa-sync"></i> Show all... (${total})</button>`).click(function(e){
           $(this).remove()
           e.preventDefault();
           while(names.length) add_one_contributor();
