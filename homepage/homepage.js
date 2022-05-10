dc3e840abe2503a4d8d919619df3ca849235da4b
Jeroen Ooms <jeroenooms@gmail.com>
2022-05-10 21:25:16 +0200

    pretty tables

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1224,6 +1224,7 @@ function populate_package_details(package){
           doc.find("h2").addClass("h4");
           doc.find("h3").addClass("h5");
           doc.find("pre").css('white-space','pre-wrap'); //for very long lines without whitespace such as 'js' package
+          doc.find("table").addClass("table table-sm");
           doc.find('img').addClass('d-none').on("load", function() {
             var img = $(this);
             /* Do not show badges and broken images */
