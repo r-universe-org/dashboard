7730baab1cce53dfa736389d1ab4e7f20720887d
Jeroen Ooms <jeroenooms@gmail.com>
2022-07-05 10:53:09 +0200

    fix galaxy

--- galaxy/galaxy.js
+++ galaxy/galaxy.js
@@ -2,15 +2,15 @@ $(function(){
   function rnd(max){
     return Math.floor(Math.random() * max);
   }
-  get_ndjson('https://r-universe.dev/stats/organizations').then(function(x){
+  get_ndjson('https://r-universe.dev/stats/universes?organization=1').then(function(x){
     const stars = $("#stars")[0];
     function order( a, b ) {
       if(a.packages.length < b.packages.length) return 1;
       if(a.packages.length > b.packages.length) return -1;
       return 0;
     }
-    x.sort(order).forEach(function(org){
-      var organization = org.organization;
+    x.sort(order).slice(0, 250).forEach(function(org){
+      var organization = org.universe;
       if(organization == 'test' || organization.includes('gitlab.com')) return;
       var size = org.packages.length;
       var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
