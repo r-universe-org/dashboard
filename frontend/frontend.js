3f8b974d044881d638b57d262ccfd71aee2eac04
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-06 11:44:20 +0200

    Try to prevent huge <pre> and tables from overflowing See https://github.com/r-universe-org/help/discussions/244

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1244,7 +1244,7 @@ function populate_readme(package){
     doc.find("h1").addClass("h3");
     doc.find("h2").addClass("h4");
     doc.find("h3").addClass("h5");
-    doc.find("table").addClass("table table-sm");
+    doc.find("table").addClass("table table-sm").attr('style', 'display: block; overflow:auto; width: 0; min-width: 100%;');
     doc.find('img').addClass('d-none').on("load", function() {
       var img = $(this);
       /* Do not show badges and broken images */
