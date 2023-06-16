4dfb9f3adbb2d823c71c723823ff1d98ad8b85b0
Jeroen Ooms <jeroenooms@gmail.com>
2023-06-16 10:52:16 +0200

    only enable json export if possible

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1297,7 +1297,7 @@ function show_data_download(x, url, package){
   $('#download-data-modal .export-xlsx').attr('href', `${server}/${package}/data/${x.name}/xlsx`).toggle(x.table === true && isdf);
   //todo: once the db is all updated, /json below requires x.json and ndjson requires isdf && x.json
   $('#download-data-modal .export-ndjson').attr('href', `${server}/${package}/data/${x.name}/ndjson`).toggle(isdf && x.tojson !== false);
-  $('#download-data-modal .export-json').attr('href', `${server}/${package}/data/${x.name}/json`).toggle(isdf || x.tojson === true);
+  $('#download-data-modal .export-json').attr('href', `${server}/${package}/data/${x.name}/json`).toggle(x.tojson !== false);
   $.get(url.replace("manual.html#", "page/"), function(str){
     var el = $.parseHTML(`<div>${str}</div>`);
     $(el).find("hr").remove();
