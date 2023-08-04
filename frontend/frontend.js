06d9ce2957cc70b68ed9439eeb4bc7443cd79148
Jeroen Ooms <jeroenooms@gmail.com>
2023-08-04 14:51:17 +0200

    hide tabs for single package page

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1735,6 +1735,12 @@ $('#tab-list .nav-item:not(.d-none) .nav-link').on('show.bs.tab', function(e){
   }
 });
 
+$('#tab-list .nav-item .nav-link').on('show.bs.tab', function(e){
+  var showtabs = $(this).attr('id') != 'package-tab-link';
+  $('#tab-list').toggleClass('d-lg-flex', showtabs).toggleClass('d-lg-none', !showtabs);
+});
+
+
 addEventListener('popstate', function(e){
   var state = e.state || {};
   //alert("pop:\n" + JSON.stringify(state))
