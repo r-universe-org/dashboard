ef74edf3c4d66ca6b29de646a30296606e24425d
Jeroen Ooms <jeroenooms@gmail.com>
2024-01-17 11:13:01 +0100

    add width toggle

--- static/manual.js
+++ static/manual.js
@@ -1,2 +1,8 @@
-/* Nothing here right now */
-
+/* Toggle between fixed and full page width */
+addEventListener("keydown", function(e){
+	if(e.key == 'w'){
+		var val = document.body.style.maxWidth ?  "" : "none";
+		console.log("Setting body max-width to", val)
+		document.body.style.maxWidth = val;
+	}
+});
